a4f9bcc6ed5f331bc06ce9ddd24e8469751c56db
Harsh-Pariya <harshpariya195@gmail.com>
2026-07-13 18:20:01 +0530

    chore: add backdrop blur to mobile navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -85,7 +85,7 @@ export function Navbar() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="fixed inset-0 top-0 z-[55] flex flex-col bg-bg px-6 pt-24 md:hidden"
+            className="fixed inset-0 top-0 z-[55] flex flex-col bg-bg/95 px-6 pt-24 backdrop-blur-md md:hidden"
             style={{ willChange: "opacity" }}
           >
 
